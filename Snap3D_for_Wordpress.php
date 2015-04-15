b4046e6ecce7adaa47ff0082187b36b1b810f012
Adam - Robin's Macbook Air <adam@evd1.tv>
2015-04-15 09:00:34 -0400

    modified "Featured Image" meta box to include Snap3D URL

--- Snap3D_for_Wordpress.php
+++ Snap3D_for_Wordpress.php
@@ -26,21 +26,10 @@
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-add_filter( 'post_thumbnail_html', 'filter_thumb', 10, 2 );
-function filter_thumb( $content ) {
-    return $content . "\n<!-- Snap3D_for_Wordpress was here. -->";
-}
 
-function new_post_thumbnail_meta_box() {
-    global $post; // we know what this does
 
-    echo '&lt;p&gt;Content above the image.&lt;/p&gt;';
 
-    $thumbnail_id = get_post_meta( $post->ID, '_thumbnail_id', true ); // grabing the thumbnail id of the post
-    echo _wp_post_thumbnail_html( $thumbnail_id ); // echoing the html markup for the thumbnail
-
-    echo '&lt;p&gt;Content below the image.&lt;/p&gt;';
-}
+// Create our own duplicate of the Featured Image meta box
 function render_new_post_thumbnail_meta_box() {
     global $post_type; // lets call the post type
 
@@ -51,5 +40,60 @@ function render_new_post_thumbnail_meta_box() {
     add_meta_box('postimagediv', __('Featured Image'), 'new_post_thumbnail_meta_box', $post_type, 'side', 'low');
 }
 add_action('do_meta_boxes', 'render_new_post_thumbnail_meta_box');
+
+
+
+
+// Glom onto our copy of the Featured Image meta box
+function new_post_thumbnail_meta_box() {
+    global $post; // we know what this does
+
+    $thumbnail_id = get_post_meta( $post->ID, '_thumbnail_id', true ); // grabing the thumbnail id of the post
+    echo _wp_post_thumbnail_html( $thumbnail_id ); // echoing the html markup for the thumbnail
+
+    wp_nonce_field( basename( __FILE__ ), 'Snap3D_nonce' );
+    $Snap3D_stored_meta = get_post_meta( $post->ID );
+
+    ?>
+    <p>
+        <label for="Snap3D-URL" class="Snap3D-row-title"><?php _e( 'Snap3D URL', 'Snap3D-textdomain' )?></label>
+        <input type="text" name="Snap3D-URL" id="Snap3D-URL" value="<?php if ( isset ( $Snap3D_stored_meta['Snap3D-URL'] ) ) echo $Snap3D_stored_meta['Snap3D-URL'][0]; ?>" />
+    </p>
+    <?php
+}
+
+
+
+
+// When post saves, save our post meta
+function Snap3D_meta_save( $post_id ) {
+
+    // Checks save status
+    $is_autosave = wp_is_post_autosave( $post_id );
+    $is_revision = wp_is_post_revision( $post_id );
+    $is_valid_nonce = ( isset( $_POST[ 'Snap3D_nonce' ] ) && wp_verify_nonce( $_POST[ 'Snap3D_nonce' ], basename( __FILE__ ) ) ) ? 'true' : 'false';
+
+    // Exits script depending on save status
+    if ( $is_autosave || $is_revision || !$is_valid_nonce ) {
+        return;
+    }
+
+    // Checks for input and sanitizes/saves if needed
+    if( isset( $_POST[ 'Snap3D-URL' ] ) ) {
+        update_post_meta( $post_id, 'Snap3D-URL', sanitize_text_field( $_POST[ 'Snap3D-URL' ] ) );
+    }
+
+}
+add_action( 'save_post', 'Snap3D_meta_save' );
+
+
+
+
+// Render thumbnail in theme
+add_filter( 'post_thumbnail_html', 'filter_thumb', 10, 2 );
+function filter_thumb( $content ) {
+    return $content . "\n<!-- Snap3D_for_Wordpress was here. -->";
+}
+
 
 ?>
